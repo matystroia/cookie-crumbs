f5f2fe171ecfea1069381032df077e90ce7cf14c
Matei <stroiamatei@gmail.com>
2019-05-06 00:54:53 +0300

    now deletes all cookies in container

--- background.js
+++ background.js
@@ -2,34 +2,46 @@ let urls = [];
 let containers = [];
 
 function handleUpdated(tabId, changeInfo, tabInfo, urlPattern) {
-	if (!(("status" in changeInfo) && tabInfo.cookieStoreId === 'firefox-default'))
-		return;
+    if (!(("status" in changeInfo) && tabInfo.cookieStoreId === 'firefox-default'))
+        return;
 
-	let container = containers[urls.indexOf(urlPattern)];
-	console.log(container);
+    let container = containers[urls.indexOf(urlPattern)];
 
-	// Close tab
-	browser.tabs.remove(tabId);
+    // Close tab
+    browser.tabs.remove(tabId);
   
-	// Re-open tab in container
-	browser.tabs.create({
-		active: tabInfo.active,
-		openerTabId: tabInfo.openerTabId,
-		cookieStoreId: container.cookieStoreId,
-		url: changeInfo.url
-	}); 
+    // Re-open tab in container
+    browser.tabs.create({
+        active: tabInfo.active,
+        index: tabInfo.index,
+        openerTabId: tabInfo.openerTabId,
+        cookieStoreId: container.cookieStoreId,
+        url: changeInfo.url
+    });
+
+    // Delete all cookies
+    browser.cookies.getAll({storeId: container.cookieStoreId}).then((cookies) => {
+        for (const cookie of cookies) {
+            browser.cookies.remove({
+                url: changeInfo.url,
+                firstPartyDomain: cookie.firstPartyDomain,
+                name: cookie.name,
+                storeId: container.cookieStoreId
+            });
+        }
+    });
 }
 
 browser.storage.sync.get("containerRules").then((rules) => {
-	let containerRules = rules.containerRules || [];
-	for (const rule of containerRules) {
-		urls.push(rule[0]);
-		browser.contextualIdentities.query({name: rule[1]}).then((identities) => {
-			containers.push(identities[0]);
-		});
-	}
+    let containerRules = rules.containerRules || [];
+    for (const rule of containerRules) {
+        urls.push(rule[0]);
+        browser.contextualIdentities.query({name: rule[1]}).then((identities) => {
+            containers.push(identities[0]);
+        });
+    }
 
-	for (const pattern of urls) {
-		browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {handleUpdated(tabId, changeInfo, tabInfo, pattern)}, {urls: [pattern]});
-	}
+    for (const pattern of urls) {
+        browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {handleUpdated(tabId, changeInfo, tabInfo, pattern)}, {urls: [pattern]});
+    }
 });

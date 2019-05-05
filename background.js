1b81e8a84d0824f960ce0564d4cdb4b36b7c8886
Matei <stroiamatei@gmail.com>
2019-05-06 01:51:45 +0300

    changed rules to js object + webstorm format

--- background.js
+++ background.js
@@ -1,54 +1,50 @@
-let patterns = [];
-let containers = [];
-let keepCookies = [];
+let rules = [];
 
-function handleUpdated(tabId, changeInfo, tabInfo, urlPattern, keepCookies) {
-	if (!(("status" in changeInfo) && tabInfo.cookieStoreId === 'firefox-default'))
-		return;
+function handleUpdated(tabId, changeInfo, tabInfo, ruleObject) {
+    if (!(("status" in changeInfo) && tabInfo.cookieStoreId === 'firefox-default'))
+        return;
 
-	let container = containers[patterns.indexOf(urlPattern)];
+    // Close tab
+    browser.tabs.remove(tabId);
 
-	// Close tab
-	browser.tabs.remove(tabId);
-  
-	// Re-open tab in container
-	browser.tabs.create({
-		active: tabInfo.active,
-		index: tabInfo.index,
-		openerTabId: tabInfo.openerTabId,
-		cookieStoreId: container.cookieStoreId,
-		url: changeInfo.url
-	});
+    // Re-open tab in container
+    browser.tabs.create({
+        active: tabInfo.active,
+        index: tabInfo.index,
+        openerTabId: tabInfo.openerTabId,
+        cookieStoreId: ruleObject.container.cookieStoreId,
+        url: changeInfo.url
+    });
 
-	if (!keepCookies) {
-		// Delete all cookies
-		browser.cookies.getAll({storeId: container.cookieStoreId}).then((cookies) => {
-			for (const cookie of cookies) {
-				browser.cookies.remove({
-					url: changeInfo.url,
-					firstPartyDomain: cookie.firstPartyDomain,
-					name: cookie.name,
-					storeId: container.cookieStoreId
-				});
-			}
-		});		
-	}
+    if (!ruleObject.keepCookies) {
+        // Delete all cookies
+        browser.cookies.getAll({storeId: ruleObject.container.cookieStoreId}).then((cookies) => {
+            for (const cookie of cookies) {
+                browser.cookies.remove({
+                    url: changeInfo.url,
+                    firstPartyDomain: cookie.firstPartyDomain,
+                    name: cookie.name,
+                    storeId: ruleObject.container.cookieStoreId
+                });
+            }
+        });
+    }
 }
 
-browser.storage.sync.get("containerRules").then((rules) => {
-	let containerRules = rules.containerRules || [];
-	for (const rule of containerRules) {
-		patterns.push(rule[0]);
-		browser.contextualIdentities.query({name: rule[1]}).then((identities) => {
-			containers.push(identities[0]);
-		});
-		keepCookies.push(rule[2]);
-	}
+browser.storage.sync.get("containerRules").then((storage) => {
+    rules = storage.containerRules || [];
 
-	// Add a handler for each rule
-	for (let i = 0; i < containerRules.length; ++i) {
-		browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
-			handleUpdated(tabId, changeInfo, tabInfo, patterns[i], keepCookies[i]);
-		}, {urls: [patterns[i]]});
-	}
+    // Add container to every rule object
+    for (let rule of rules) {
+        browser.contextualIdentities.query({name: rule.containerName}).then((identities) => {
+            rule.container = identities[0];
+        });
+    }
+
+    // Add a handler for each rule
+    for (const rule of rules) {
+        browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+            handleUpdated(tabId, changeInfo, tabInfo, rule);
+        }, {urls: [rule.urlPattern]});
+    }
 });
--- settings.js
+++ settings.js
@@ -1,66 +1,67 @@
 let containerRules;
 let table = document.getElementById("table");
 
-function addRule(e) {
-	containerRules.push([
-		document.querySelector("#url").value,
-		document.querySelector("#container").value,
-		document.querySelector("#keepCookies").checked
-		]);
-	saveOptions();
-	setRuleTable();
+function addRule() {
+    containerRules.push({
+        urlPattern: document.querySelector("#url").value,
+        containerName: document.querySelector("#container").value,
+        keepCookies: document.querySelector("#keepCookies").checked
+    });
+
+    saveOptions();
+    setRuleTable();
 }
 
 function saveOptions() {
-	browser.storage.sync.set({
-		containerRules: containerRules
-	});
+    browser.storage.sync.set({
+        containerRules: containerRules
+    });
 }
 
 function removeRow(event) {
-	// Remove from array
-	containerRules.splice(parseInt(event.target.getAttribute("data-id")), 1);
-	saveOptions();
-	setRuleTable();
+    // Remove from array
+    containerRules.splice(parseInt(event.target.getAttribute("data-id")), 1);
+    saveOptions();
+    setRuleTable();
 }
 
 function setRuleTable() {
-	let newTable = document.createElement("tbody");
-	let index = 0;
-	for (const rule of containerRules) {
-		let newRow = newTable.insertRow(-1);
+    let newTable = document.createElement("tbody");
+    let index = 0;
+    for (const rule of containerRules) {
+        let newRow = newTable.insertRow(-1);
 
-		let urlCell = newRow.insertCell(0);
-		let containerCell = newRow.insertCell(1);
-		let keepCell = newRow.insertCell(2)
-		let removeCell = newRow.insertCell(3);
+        let urlCell = newRow.insertCell(0);
+        let containerCell = newRow.insertCell(1);
+        let keepCell = newRow.insertCell(2);
+        let removeCell = newRow.insertCell(3);
 
-		urlCell.appendChild(document.createTextNode(rule[0]));
-		containerCell.appendChild(document.createTextNode(rule[1]));
+        urlCell.appendChild(document.createTextNode(rule.urlPattern));
+        containerCell.appendChild(document.createTextNode(rule.containerName));
 
-		let checkbox = document.createElement("input");
-		checkbox.type = "checkbox";
-		checkbox.checked = rule[2];
-		keepCell.appendChild(checkbox);
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = rule.keepCookies;
+        keepCell.appendChild(checkbox);
 
-		let button = document.createElement("button");
-		button.setAttribute("data-id", index.toString());
-		button.innerHTML = "Remove";
-		button.addEventListener("click", removeRow);
-		removeCell.appendChild(button);
+        let button = document.createElement("button");
+        button.setAttribute("data-id", index.toString());
+        button.innerHTML = "Remove";
+        button.addEventListener("click", removeRow);
+        removeCell.appendChild(button);
 
-		index++;
-	}
-	
-	table.parentNode.replaceChild(newTable, table);
-	table = newTable;
+        index++;
+    }
+
+    table.parentNode.replaceChild(newTable, table);
+    table = newTable;
 }
 
 function restoreOptions() {
-	browser.storage.sync.get("containerRules").then((rules) => {
-		containerRules = rules.containerRules || [];
-		setRuleTable();
-	});
+    browser.storage.sync.get("containerRules").then((rules) => {
+        containerRules = rules.containerRules || [];
+        setRuleTable();
+    });
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);

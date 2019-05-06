e9eecad048a888aa6b3776ca1acbe7b1d65ce861
Matei <stroiamatei@gmail.com>
2019-05-06 03:20:31 +0300

    function to toggle tab container

--- background.js
+++ background.js
@@ -1,20 +1,29 @@
 let rules = [];
+let whitelistedTabs = [];
+
+function moveTabToContainer(tab, cookieStoreId) {
+    // Close tab
+    browser.tabs.remove(tab.id);
+
+    // Re-open tab in container
+    browser.tabs.create({
+        active: tab.active,
+        cookieStoreId: cookieStoreId,
+        index: tab.index,
+        openerTabId: tab.openerTabId,
+        url: tab.url,
+        windowId: tab.windowId
+    });
+}
 
 function handleUpdated(tabId, changeInfo, tabInfo, ruleObject) {
     if (!(("status" in changeInfo) && tabInfo.cookieStoreId === 'firefox-default'))
         return;
 
-    // Close tab
-    browser.tabs.remove(tabId);
+    if (whitelistedTabs.includes(tabInfo.successorId))
+        return;
 
-    // Re-open tab in container
-    browser.tabs.create({
-        active: tabInfo.active,
-        index: tabInfo.index,
-        openerTabId: tabInfo.openerTabId,
-        cookieStoreId: ruleObject.container.cookieStoreId,
-        url: changeInfo.url
-    });
+    moveTabToContainer(tabInfo, ruleObject.container.cookieStoreId);
 
     if (!ruleObject.keepCookies) {
         // Delete all cookies
@@ -29,6 +38,25 @@ function handleUpdated(tabId, changeInfo, tabInfo, ruleObject) {
             }
         });
     }
+}
+
+function toggleActiveTabContainer() {
+    // Get active tab
+    browser.tabs.query({active: true}).then((tabs) => {
+        // Discard tabs with default container
+        while (tabs[0].cookieStoreId === 'firefox-default' && !whitelistedTabs.includes(tabs[0].successorId))
+            tabs.shift();
+
+        // TODO: This won't work if tabs are sorted by container
+        let index = whitelistedTabs.indexOf(tabs[0].successorId);
+        if (index > -1)
+            whitelistedTabs.splice(index, 1);
+        else {
+            whitelistedTabs.push(tabs[0].succesorId);
+            // Re-open in default container
+            moveTabToContainer(tabs[0], 'firefox-default');
+        }
+    });
 }
 
 browser.storage.sync.get("containerRules").then((storage) => {
